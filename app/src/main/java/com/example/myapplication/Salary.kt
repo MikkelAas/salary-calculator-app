2fbda1598a1a5ab76cf7cab14fe651ba4f95c50f
Mikkel Aas <mikkel.aas@schibsted.com>
2023-10-08 17:13:47 +0200

    update salary class

--- app/src/main/java/com/example/myapplication/Salary.kt
+++ app/src/main/java/com/example/myapplication/Salary.kt
@@ -1,50 +1,31 @@
 package com.example.myapplication
 
 data class Salary(
-    var baseSalary: Float = 0F,
+    var hourlyWage: Float = 0F,
     var tax: Float? = null,
-    var normalHours: Float = 0F,
-    var fiftyPercentHours: Float = 0F,
-    var hundredPercentHours: Float = 0F,
-    var oneFiftyPercentHours: Float = 0F,
+    var weekendWage: Float = 0F,
+    var nightWage: Float = 0F,
+    var workTimeRecords: MutableList<WorkTimeRecord> = mutableListOf()
 ) {
-
     fun calculateTotalSalary(): Float {
-        val normalSalaryTotal = calculateNormalSalary()
+        var total = 0F
 
-        val bonusSalaryTotal = calculateTotalBonus()
+        for (workTimeRecord in workTimeRecords) {
+            var bonus = workTimeRecord.bonusInPercent
 
-        val totalSalaryBeforeTax = normalSalaryTotal + bonusSalaryTotal
+            bonus = if (bonus != null) {
+                (bonus + 100) / 100
+            } else {
+                1F
+            }
 
-        if (tax != null) {
-            return totalSalaryBeforeTax * ((100 - tax!!) / 100)
+            total += workTimeRecord.hours * (bonus) * hourlyWage + (workTimeRecord.weekendHours * weekendWage) + (workTimeRecord.nightHours * nightWage)
         }
 
-        return totalSalaryBeforeTax
-    }
+        if (tax != null) {
+            return total * ((100 - tax!!) / 100)
+        }
 
-    private fun calculateNormalSalary(): Float {
-        return baseSalary * normalHours
+        return total
     }
-
-    private fun calculateTotalBonus(): Float {
-        return (
-                calculateBonusSalary(fiftyPercentHours, BonusPercent.FIFTY) +
-                        calculateBonusSalary(hundredPercentHours, BonusPercent.HUNDRED) +
-                        calculateBonusSalary(oneFiftyPercentHours, BonusPercent.ONE_FIFTY)
-                )
-    }
-
-    private fun calculateBonusSalary(
-        bonusPercentHours: Float,
-        bonusPercent: BonusPercent
-    ): Float {
-        return bonusPercentHours * (bonusPercent.value * baseSalary)
-    }
-}
-
-enum class BonusPercent(val value: Float) {
-    FIFTY(1.5F),
-    HUNDRED(2F),
-    ONE_FIFTY(2.5F)
 }
