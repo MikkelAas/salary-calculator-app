08878725692b4438fe2fd2714db5dfa8100f50d9
Mikkel Aas <mikkel.aas@schibsted.com>
2023-10-08 17:14:13 +0200

    update tests

--- app/src/test/java/com/example/myapplication/SalaryTest.kt
+++ app/src/test/java/com/example/myapplication/SalaryTest.kt
@@ -7,10 +7,16 @@ import org.junit.Assert.*
 class SalaryTest {
 
     @Test
-    fun `calculate salary with only normal hours`() {
+    fun `calculate salary with only one set of normal hours`() {
+        val workTimeRecords = mutableListOf(
+            WorkTimeRecord(
+                hours = 10F
+            )
+        )
+
         val salary = Salary(
-            baseSalary = 100F,
-            normalHours = 10F,
+            hourlyWage = 100F,
+            workTimeRecords = workTimeRecords
         )
 
         val totalSalary = salary.calculateTotalSalary()
@@ -19,22 +25,17 @@ class SalaryTest {
     }
 
     @Test
-    fun `calculate salary with only fifty percent hours`() {
-        val salary = Salary(
-            baseSalary = 100F,
-            fiftyPercentHours = 10F,
+    fun `calculate salary with two sets of normal hours`() {
+        val workTimeRecords = mutableListOf(
+            WorkTimeRecord(
+                hours = 10F
+            ),
+            WorkTimeRecord(10F)
         )
 
-        val totalSalary = salary.calculateTotalSalary()
-
-        assertEquals(1500F, totalSalary)
-    }
-
-    @Test
-    fun `calculate salary with only hundred percent hours`() {
         val salary = Salary(
-            baseSalary = 100F,
-            hundredPercentHours = 10F,
+            hourlyWage = 100F,
+            workTimeRecords = workTimeRecords
         )
 
         val totalSalary = salary.calculateTotalSalary()
@@ -43,45 +44,93 @@ class SalaryTest {
     }
 
     @Test
-    fun `calculate salary with only one fifty percent hours`() {
+    fun `calculate salary with bonus hours`() {
+        val workTimeRecords = mutableListOf(
+            WorkTimeRecord(
+                hours = 10F,
+                bonusInPercent = 30F
+            ),
+            WorkTimeRecord(
+                hours = 5F,
+                bonusInPercent = 50F
+            )
+        )
+
         val salary = Salary(
-            baseSalary = 100F,
-            oneFiftyPercentHours = 10F,
+            hourlyWage = 100F,
+            workTimeRecords = workTimeRecords
         )
 
         val totalSalary = salary.calculateTotalSalary()
 
-        assertEquals(2500F, totalSalary)
+        assertEquals(2050F, totalSalary)
     }
 
     @Test
-    fun `calculate salary with all types of hours`() {
+    fun `calculate salary with bonus hours and weekend bonus`() {
+        val workTimeRecords = mutableListOf(
+            WorkTimeRecord(
+                hours = 10F,
+                bonusInPercent = 50F,
+                weekendHours = 5F
+            ),
+        )
+
         val salary = Salary(
-            baseSalary = 100F,
-            normalHours = 10F,
-            fiftyPercentHours = 10F,
-            hundredPercentHours = 10F,
-            oneFiftyPercentHours = 10F,
+            hourlyWage = 100F,
+            workTimeRecords = workTimeRecords,
+            weekendWage = 100F
         )
 
         val totalSalary = salary.calculateTotalSalary()
 
-        assertEquals(7000F, totalSalary)
+        assertEquals(2000F, totalSalary)
     }
 
     @Test
-    fun `calculate salary after tax`() {
+    fun `calculate with everything`() {
+        val workTimeRecords = mutableListOf(
+            WorkTimeRecord(
+                hours = 7F,
+                bonusInPercent = 50F,
+                weekendHours = 4F,
+                nightHours = 3F
+            ),
+        )
+
         val salary = Salary(
-            baseSalary = 100F,
-            tax = 10F,
-            normalHours = 10F,
-            fiftyPercentHours = 10F,
-            hundredPercentHours = 10F,
-            oneFiftyPercentHours = 10F,
+            hourlyWage = 100F,
+            tax = 30F,
+            workTimeRecords = workTimeRecords,
+            weekendWage = 100F,
+            nightWage = 50F
         )
 
         val totalSalary = salary.calculateTotalSalary()
 
-        assertEquals(6300F, totalSalary)
+        assertEquals(1120F, totalSalary)
+    }
+
+    @Test
+    fun `calculate before tax`() {
+        val workTimeRecords = mutableListOf(
+            WorkTimeRecord(
+                hours = 7F,
+                bonusInPercent = 50F,
+                weekendHours = 4F,
+                nightHours = 3F
+            ),
+        )
+
+        val salary = Salary(
+            hourlyWage = 100F,
+            workTimeRecords = workTimeRecords,
+            weekendWage = 100F,
+            nightWage = 50F
+        )
+
+        val totalSalary = salary.calculateTotalSalary()
+
+        assertEquals(1600F, totalSalary)
     }
 }
