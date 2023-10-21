026671aa6a432a78301a42dc7bf1e0dc5fcf4d08
Mikkel Aas <mikkel.aas@schibsted.com>
2023-10-21 22:55:11 +0200

    remove outer box layer

--- app/src/main/java/com/example/myapplication/screens/SalaryInfoScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/SalaryInfoScreen.kt
@@ -1,12 +1,13 @@
 package com.example.myapplication.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,48 +17,50 @@ import com.example.myapplication.components.NumberInput
 @Composable
 fun SalaryInfoScreen() {
 
-    Box {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.height(16.dp))
+    val scrollState = rememberScrollState()
 
-            NumberInput(
-                label = "Timelønn",
-                value = "",
-                onValueChange = {
-                }, supportingText = "Timelønn i NOK"
-            )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .verticalScroll(state = scrollState),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
 
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
 
-            NumberInput(
-                label = "Skatt",
-                value = "",
-                onValueChange = {
-                },
-                supportingText = "Skatt i %"
-            )
-
-            NumberInput(
-                label = "Helgetillegg",
-                value = "",
-                onValueChange = {
-
-                }, supportingText = "Helgetillegg i NOK"
-            )
+        NumberInput(
+            label = "Timelønn",
+            value = "",
+            onValueChange = {
+            }, supportingText = "Timelønn i NOK"
+        )
 
 
-            NumberInput(
-                label = "Kvelds- og nattillegg",
-                value = "",
-                onValueChange = {
-                },
-                supportingText = "Kvelds- og nattillegg i NOK"
-            )
-        }
+        NumberInput(
+            label = "Skatt",
+            value = "",
+            onValueChange = {
+            },
+            supportingText = "Skatt i %"
+        )
+
+        NumberInput(
+            label = "Helgetillegg",
+            value = "",
+            onValueChange = {
+
+            }, supportingText = "Helgetillegg i NOK"
+        )
+
+
+        NumberInput(
+            label = "Kvelds- og nattillegg",
+            value = "",
+            onValueChange = {
+            },
+            supportingText = "Kvelds- og nattillegg i NOK"
+        )
     }
 }
