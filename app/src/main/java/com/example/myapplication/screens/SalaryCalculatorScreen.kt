271a8cbde03f462c3ae3bd190e571cf3b35c7b93
Mikkel Aas <mikkel.aas@schibsted.com>
2023-10-23 20:18:18 +0200

    use lazyColumn instead

--- app/src/main/java/com/example/myapplication/screens/SalaryCalculatorScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/SalaryCalculatorScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -64,23 +66,19 @@ fun SalaryCalculatorScreen(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(5.dp))
 
-        Column {
-
-            for (workTimeRecord in workTimeRecords) {
+        LazyColumn() {
+            items(workTimeRecords) {
                 WorkHoursCard(
                     onHoursInputChange = {},
                     onBonusInputChange = {},
                     onWeekendHoursInputChange = {},
                     onNightHoursInputChange = {},
                     deleteButtonOnClick = {
-                        workTimeRecords.remove(workTimeRecord)
+                        workTimeRecords.remove(it)
                     }
                 )
-
-                Spacer(modifier = Modifier.height(16.dp))
             }
         }
-
 
         Spacer(modifier = Modifier.height(16.dp))
 
