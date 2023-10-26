6f5725f3870e6b1a211bdb2fb51e17ebda1652ee
Mikkel Aas <mikkel.aas@schibsted.com>
2023-10-26 21:25:24 +0200

    center aligned top bar

--- app/src/main/java/com/example/myapplication/components/SalaryCalculatorTopAppBar.kt
+++ app/src/main/java/com/example/myapplication/components/SalaryCalculatorTopAppBar.kt
@@ -1,9 +1,9 @@
 package com.example.myapplication.components
 
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.style.TextAlign
@@ -11,7 +11,7 @@ import androidx.compose.ui.text.style.TextAlign
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SalaryCalculatorTopAppBar() {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.smallTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             titleContentColor = MaterialTheme.colorScheme.primary
