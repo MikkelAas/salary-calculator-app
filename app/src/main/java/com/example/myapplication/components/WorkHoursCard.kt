c037b6a1882ac2b1c092358a086fe64a0d195d3c
Mikkel Aas <mikkel.aas@schibsted.com>
2023-10-26 22:02:09 +0200

    use remember instead of rememberSavable

--- app/src/main/java/com/example/myapplication/components/WorkHoursCard.kt
+++ app/src/main/java/com/example/myapplication/components/WorkHoursCard.kt
@@ -19,7 +19,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,9 +42,9 @@ fun WorkHoursCard(
 
     val calendar = Calendar.getInstance()
 
-    var selectedDateText by rememberSaveable { mutableStateOf("Velg dato") }
+    var selectedDateText by remember { mutableStateOf("Velg dato") }
 
-    var isExpanded by rememberSaveable {
+    var isExpanded by remember {
         mutableStateOf(true)
     }
 
