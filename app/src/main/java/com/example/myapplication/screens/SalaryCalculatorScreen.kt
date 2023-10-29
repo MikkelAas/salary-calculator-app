d4704e0bd40413dedffcdfa16e43f03c33955004
Mikkel Aas <mikkel.aas@schibsted.com>
2023-10-29 16:55:19 +0100

    use scaffold to create floating action button

--- app/src/main/java/com/example/myapplication/screens/SalaryCalculatorScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/SalaryCalculatorScreen.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,6 +32,7 @@ import com.example.myapplication.dialogs.TotalSalaryDialog
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.viewModels.SalaryCalculatorViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SalaryCalculatorScreen(
     salaryCalculatorViewModel: SalaryCalculatorViewModel
@@ -40,59 +43,58 @@ fun SalaryCalculatorScreen(
 
     val blurRadius = if (showDialog) 10.dp else 0.dp
 
-    FloatingActionButton(
-        onClick = {
-            salaryCalculatorViewModel.workTimeRecords.add(WorkTimeRecord())
-        }) {
-        Icon(Icons.Filled.Add, "Add new work time record.")
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp)
-            .blur(blurRadius),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        if (salaryCalculatorViewModel.workTimeRecords.isNotEmpty()) {
-            CalculateButton(
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
                 onClick = {
-                    salaryCalculatorViewModel.totalSalary =
-                        salaryCalculatorViewModel.calculateTotalSalary()
-                    showDialog = !showDialog
-                },
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        LazyColumn() {
-            items(salaryCalculatorViewModel.workTimeRecords) { workTimeRecord ->
-                WorkHoursCard(
-                    workTimeRecord = workTimeRecord,
-                    onHoursInputChange = {
-                        workTimeRecord.hours = it.toFloatOrNull() ?: 0F
+                    salaryCalculatorViewModel.workTimeRecords.add(WorkTimeRecord())
+                }) {
+                Icon(Icons.Filled.Add, "Add new work time record.")
+            }
+        },
+    ) { innerPadding ->
+        Column {
+            if (salaryCalculatorViewModel.workTimeRecords.isNotEmpty()) {
+                CalculateButton(
+                    onClick = {
+                        salaryCalculatorViewModel.totalSalary =
+                            salaryCalculatorViewModel.calculateTotalSalary()
+                        showDialog = !showDialog
                     },
-                    onBonusInputChange = {
-                        workTimeRecord.bonusInPercent = it.toFloatOrNull()
-                    },
-                    onWeekendHoursInputChange = {
-                        workTimeRecord.weekendHours = it.toFloatOrNull() ?: 0F
-                    },
-                    onNightHoursInputChange = {
-                        workTimeRecord.nightHours = it.toFloatOrNull() ?: 0F
-                    },
-                    deleteButtonOnClick = {
-                        salaryCalculatorViewModel.workTimeRecords.remove(workTimeRecord)
-                    }
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
+                    .blur(blurRadius)
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(salaryCalculatorViewModel.workTimeRecords) { workTimeRecord ->
+                    WorkHoursCard(
+                        workTimeRecord = workTimeRecord,
+                        onHoursInputChange = {
+                            workTimeRecord.hours = it.toFloatOrNull() ?: 0F
+                        },
+                        onBonusInputChange = {
+                            workTimeRecord.bonusInPercent = it.toFloatOrNull()
+                        },
+                        onWeekendHoursInputChange = {
+                            workTimeRecord.weekendHours = it.toFloatOrNull() ?: 0F
+                        },
+                        onNightHoursInputChange = {
+                            workTimeRecord.nightHours = it.toFloatOrNull() ?: 0F
+                        },
+                        deleteButtonOnClick = {
+                            salaryCalculatorViewModel.workTimeRecords.remove(workTimeRecord)
+                        }
+                    )
 
-                Spacer(modifier = Modifier.height(10.dp))
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
             }
         }
 
